4cdb396c75f52522300f1bd3aa3bb0bfca3e3123
itg <itg@DESKTOP-3AJ6CDP>
2025-01-20 11:39:00 +0900

    로그인 관련 추가

--- src/main/java/com/itg/examp/MemberController.java
+++ src/main/java/com/itg/examp/MemberController.java
@@ -1,17 +1,21 @@
 package com.itg.examp;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.itg.examp.dao.MemberDAO;
 import com.itg.examp.dto.MemberDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
 
 @RestController
 @RequestMapping("/member")
@@ -22,34 +26,47 @@ public class MemberController {
 	MemberService service;
 	@PostMapping("/signup")
 	public Map<String, Object> signupMember(MemberDTO member) {
-		// id 는 4글자이상, 비밀번호는 6글자 이상
-		
+		// id 는 4 글자이상 pw 6글자 이상으로 입력되게		
 		System.out.println(member.toString());
-		
 		boolean ckid = service.checkId(member.getMid());
 		boolean ckpw = service.checkPassword(member.getMpw());
 		HashMap<String, Object> hm = new HashMap<>();
 		if(!ckid) {
-			hm.put("message", "아이디를 점검하세요.");
+			hm.put("message", "아이디를 점검하세요");
 		}
 		if(!ckpw) {
-			hm.put("message", "비밀번호를 점검하세요.");
+			hm.put("message", "비밀번호를 점검하세요");
 		}
-		
 		int res = dao.signupMember(member);
-		if(res == 1) {
+		if(res==1) {
 			member.setMpw("");
 			hm.put("message", "회원가입성공");
 			hm.put("data", member);
-			System.out.println(member.getName()+" 회원 가입 성공");
+			System.out.println(member.getName()+" 회원 가입성공");
+			
 		}else {
 			hm.put("message", "회원가입실패 관리자 문의");
-			System.out.println("회원가입 실패");
+			System.out.println("회원가입실패");
 		}
 		return hm;
 	}
 	@PostMapping("/login")
-	public void signin() {}
+	public Map signin(HttpServletRequest request, @RequestBody Map<String,String> logindata) {
+		System.out.println(logindata);
+		HashMap<String, Object> hm = new HashMap<>();
+		//로그인 검증
+		MemberDTO member = dao.signinMember(logindata);
+		if(member!=null) {
+			HttpSession auth =  request.getSession(true);
+			auth.setAttribute("mid", member.getMid()); // 세션에 아이디 설정
+//			auth.getAttribute("mid"); // 세션에서 아이디 출력
+			hm.put("message", "로그인성공");
+			hm.put("member", member);
+		}else {
+			hm.put("message", "아이디와 비밀번호를 다시 확인하세요.");
+		}
+		return hm;		
+	}
 	@GetMapping("/logout")
 	public void signout() {}
 	@GetMapping("/listview")
--- src/main/java/com/itg/examp/dao/MemberDAOImpl.java
+++ src/main/java/com/itg/examp/dao/MemberDAOImpl.java
@@ -21,7 +21,8 @@ public class MemberDAOImpl implements MemberDAO{
 	}
 	@Override
 	public MemberDTO signinMember(Map<String, String> data) {
-		return null;
+		MemberDTO member = session.selectOne("memberMapper.signin", data);
+		return member;
 	}
 	@Override
 	public List<MemberDTO> memberList(){
